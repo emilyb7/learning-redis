9f29f259f26936690de3cfe53913c966b160908c
emily <limeyb7@gmail.com>
2017-02-23 12:21:57 +0000

    refactors script a bit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,37 @@
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      if (req.status == 200) { resolve(req.response); }
+      else { reject(Error(req.statusText)); }
+    };
+
+    req.onerror = function() { reject(Error("Network Error")); };
+
+    req.send();
+  });
+}
+
+get('/items').then(function(response) {
+  var data = JSON.parse(response);
+  var items = data.items;
+  var scores = data.scores;
+  var itemList = items.filter(function(item) {
+    return !!item;
+  }).map(function(item, index){
+    var listItem = document.createElement('li');
+    listItem.textContent = item;
+    listItem.id = scores[index];
+    listItem.addEventListener("click", remove);
+    return listItem;
+  })
+  itemList.forEach(function(item) { document.querySelector('ul').appendChild(item); });
+}, function(error) {
+  console.error("Failed!", error);
+})
+
 function remove () {
   var item = this.id;
   var list =  document.querySelector('ul');
@@ -18,33 +52,10 @@ function remove () {
           list.appendChild(listItem);
           listItem.addEventListener("click", remove);
         }
-      }); 
+      });
 
     }
   }
   removeRequest.open("POST", "/remove", true);
   removeRequest.send(JSON.stringify({ item: item, }));
 }
-
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    var data = JSON.parse(xhr.responseText);
-    var items = data.items;
-    var scores = data.scores;
-    var list = document.querySelector('ul');
-    items.forEach(function(item, index) {
-      if (item) {
-        var listItem = document.createElement('li');
-        listItem.textContent = item;
-        listItem.id = scores[index];
-        list.appendChild(listItem);
-        listItem.addEventListener("click", remove);
-      }
-    });
-  }
-}
-
-xhr.open("GET", "/items");
-xhr.send();
